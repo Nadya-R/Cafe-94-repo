f1663e1aa906f0843c1223745509c7493aa903e5
nalRod <nal.roderick@outlook.com>
2024-04-28 00:23:59 +0100

    created a manager profile GUI, linked t to the rest of the System. one controller for the top pane one for the bottom. Created basic classes for Chef and Waiter

--- Waiter.java
+++ Waiter.java
@@ -1,4 +1,6 @@
-////public class Waiter extends Staff {
-    
-    //create Order
-//}
+public class Waiter extends Staff {
+
+    public Waiter(String firstName, String lastName, String address, String postCode) {
+        super(firstName, lastName, address, postCode);
+    }
+}
