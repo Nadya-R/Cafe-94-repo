e76804255823760700840d7ce5f9b85307b863b8
nalRod <nal.roderick@outlook.com>
2024-04-28 21:24:11 +0100

    Added action to the add staff button which pulls through staff role

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class Main extends Application {
 
     @Override
-    public void start (Stage stage) {
+    public void start(Stage stage) {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("Welcome.fxml"));
             Scene scene = new Scene(root);
@@ -21,7 +21,25 @@ public class Main extends Application {
         }
     }
 
-    public static void main (String [] args) {
+    //method to open the Create Staff GUI
+    public static void openCreateStaffScene(String role) {
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("CreateStaffScene.fxml"));
+            Parent root = loader.load();
+
+            CreateStaffController controller = loader.getController();
+            controller.setStaffRole(role); // Pass the role to the controller
+
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
         launch(args);
     }
 }
--- Staff.java
+++ Staff.java
@@ -1,12 +1,17 @@
 //this will provide a list of hours worked.
-//a function to add hours worked 
+//a function to add hours worked
 //view of how many hours work out agreed hours
 //to work
 
 //each member of staff will be able to access use addHours function etc.
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class Staff extends User {
+    //declaring a list to store all instances of staff
+    private static List<Staff> staffList = new ArrayList<>();
+
     private double hoursToWork;
     // private double shiftHours; //may not be necessary
 
@@ -14,22 +19,43 @@ public class Staff extends User {
     private ArrayList<Double> hoursWorked;
     private ArrayList<Double> overTimeWorked;
 
-        //default constructor in order to extend to subclasses
+    //default constructor in order to extend to subclasses
     public Staff() {
         // Default constructor
     }
 
-    public Staff (String firstName, String lastName, String address, String postCode) {
-        super(firstName, lastName, address, postCode);;
-    }
-
     // constructor in order to use this class as a super
-    //public Staff (String firstName, String lastName, String address, String postCode) {
-       // super(firstName, lastName, address, postCode);;
+    public Staff(String firstName, String lastName, String address, String postCode) {
+        super(firstName, lastName, address, postCode);
+        ;
         //hoursWorked = new ArrayList<>();
         //overTimeWorked = new ArrayList<>();
-   // }
+    }
 
+    // Method to add a staff member to the list
+    public static void addStaffMember(Staff staffMember) {
+        staffList.add(staffMember);
+    }
+
+    // Method to remove a staff member from the list
+    public static void removeStaffMember(Staff staffMember) {
+        staffList.remove(staffMember);
+    }
+
+    // Method to get the list of staff members
+    public static List<Staff> getStaffList() {
+        return staffList;
+    }
+
+    public static void showStaffList() {
+        System.out.println("Staff List:");
+        for (Staff staffMember : staffList) {
+            System.out.println("Name: " + staffMember.getFirstName() + " " + staffMember.getLastName());
+            System.out.println("Address: " + staffMember.getAddress());
+            System.out.println("Post Code: " + staffMember.getPostCode());
+        }
+
+    }
 
     // set hours to work for the month or whatever
     public void setHours(double hours) {
@@ -43,19 +69,19 @@ public class Staff extends User {
         hoursWorked.add(hours);
         //double sum = 0.0;
         // tally total in
-       // for (double num : hoursWorked) {
-           // sum += num;
+        // for (double num : hoursWorked) {
+        // sum += num;
 
-            //if (sum >= hoursToWork) {
-              //  overTimeWorked.add(hours);
-            //} else {
-              //  hoursWorked.add(hours);
-            //}
+        //if (sum >= hoursToWork) {
+        //  overTimeWorked.add(hours);
+        //} else {
+        //  hoursWorked.add(hours);
+        //}
         //}
     }
 
     //methid to get total hours worked
-    public double getTotalHoursWorked () {
+    public double getTotalHoursWorked() {
         double totalHours = 0.0;
         for (double num : hoursWorked) {
             totalHours += num;
@@ -63,8 +89,8 @@ public class Staff extends User {
         return totalHours;
     }
 
-    //total hours worked 
-    public String toString1(){
+    //total hours worked
+    public String toString1() {
         String str = "total hours worked ";
         return str + getTotalHoursWorked();
     }
